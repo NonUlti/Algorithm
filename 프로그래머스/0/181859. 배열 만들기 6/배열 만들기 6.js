93deb803a19e0c1f9a1a2e9c22d581410a4878fa
Choi Junsung <77382646+NonUlti@users.noreply.github.com>
2025-11-04 00:57:55 +0900

    [level 0] Title: 배열 만들기 6, Time: 10.10 ms, Memory: 63.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,22 @@
+function solution(arr) {
+    var answer = [];
+    let i=0;
+    while (i<arr.length) {
+        if (answer.length === 0) {
+            answer.push(arr[i]);
+            i += 1;
+        } else {
+            if (answer[answer.length-1] === arr[i]) {
+                answer.pop();
+                i += 1;
+            } else {
+                answer.push(arr[i]);
+                i += 1;
+            }
+        }
+    }
+    if (answer.length === 0) {
+        answer.push(-1)
+    }
+    return answer;
+}
